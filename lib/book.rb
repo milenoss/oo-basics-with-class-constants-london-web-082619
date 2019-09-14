ca4d1a3023a8de04e9019eb3598102a45f76dcd8
Milen Khanal <milenkhanal@gmail.com>
2019-09-14 17:57:40 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,18 +1,38 @@
 class Book
-  attr_accessor :author, :page_count
-  attr_reader :title,:genre
-  GENRES = []
-  def initialize(title , genre)
-    @title = title
-    @genre = genre
-  end
-
-  def turn_page
-    puts "Flipping the page...wow, you read fast!"
-  end
- 
- def genre=(genre)
-   @genre = genre
-   GENRES << genre 
- end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Book
+#   attr_accessor :author, :page_count
+#   attr_reader :title,:genre
+#   GENRES = []
+#   def initialize(title , genre)
+#     @title = title
+#     @genre = genre
+#   end
+
+#   def turn_page
+#     puts "Flipping the page...wow, you read fast!"
+#   end
+ 
+# def genre=(genre)
+#   @genre = genre
+#   GENRES << genre 
+# end
+# end
